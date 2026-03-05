d3ae81c09c813b63c8951d551920f872e8918f66
Alfonso Rodríguez Peña Monterrubio <arodriguezpenam@gmail.com>
2026-03-05 21:08:09 +0000

    File changes

--- src/components/admin/TabNavGroups.jsx
+++ src/components/admin/TabNavGroups.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ChevronDown, ChevronRight } from 'lucide-react';
 
