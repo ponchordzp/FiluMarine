88da52252658284bc10d8478c8c6bd534e90bdd9
Alfonso Rodríguez Peña Monterrubio <arodriguezpenam@gmail.com>
2026-03-19 18:23:26 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
+import LanguageSwitcher from '@/components/home/LanguageSwitcher'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
