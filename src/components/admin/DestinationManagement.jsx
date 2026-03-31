f491cef2bffa229dbd1c2169071e87c3e9b68880
Alfonso Rodríguez Peña Monterrubio <arodriguezpenam@gmail.com>
2026-03-31 16:44:37 +0000

    File changes

--- src/components/admin/DestinationManagement.jsx
+++ src/components/admin/DestinationManagement.jsx
@@ -182,6 +182,7 @@ export default function DestinationManagement({ operatorFilter = 'all', location
                     <SelectContent>
                       <SelectItem value="ixtapa_zihuatanejo">Ixtapa-Zihuatanejo</SelectItem>
                       <SelectItem value="acapulco">Acapulco</SelectItem>
+                      <SelectItem value="cancun">Cancún</SelectItem>
                     </SelectContent>
                   </Select>
                 </div>
